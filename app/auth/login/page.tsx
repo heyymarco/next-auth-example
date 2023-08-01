1e745d2e31cd8d14ae2818642aac71bf9f1b4799
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-08-01 14:14:27 +0700

    updated login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,12 +5,14 @@ import { default as React, useEffect, useState } from 'react'
 import { signIn } from 'next-auth/react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { ModalStatus } from '@/components/ModalStatus'
+import { AccessibilityProvider } from '@reusable-ui/core'
 
 
 
 function TabLogin() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    let   [busy, setBusy] = useState(false);
     const [error, setError] = useState('');
     
     const searchParams = useSearchParams();
@@ -23,27 +25,31 @@ function TabLogin() {
     
     return (
         <div>
-            <TextInput placeholder='Username or Email' value={username} onChange={({target: {value}}) => setUsername(value)} />
-            <PasswordInput placeholder='Password' value={password} onChange={({target: {value}}) => setPassword(value)} />
-            <Button onClick={async () => {
-                const result = await signIn('credentials', { username, password, callbackUrl: '/', redirect: false });
-                if (!result) return;
-                if (result.error) {
-                    setError(result.error);
-                }
-                else {
-                    router.replace('/');
-                }
-            }}>
-                Login
-            </Button>
-            <hr />
-            <Button onClick={() => signIn('facebook', { callbackUrl: '/' })}>
-                Login with Facebook
-            </Button>
-            <Button onClick={() => signIn('github', { callbackUrl: '/' })}>
-                Login with Github
-            </Button>
+            <AccessibilityProvider enabled={!busy}>
+                <TextInput placeholder='Username or Email' value={username} onChange={({target: {value}}) => setUsername(value)} />
+                <PasswordInput placeholder='Password' value={password} onChange={({target: {value}}) => setPassword(value)} />
+                <ButtonIcon icon={busy ? 'busy' : 'login'} onClick={async () => {
+                    setBusy(busy = true);
+                    const result = await signIn('credentials', { username, password, callbackUrl: '/', redirect: false });
+                    if (!result || result.error) setBusy(busy = false);
+                    if (!result) return;
+                    if (result.error) {
+                        setError(result.error);
+                    }
+                    else {
+                        router.replace('/');
+                    }
+                }}>
+                    Login
+                </ButtonIcon>
+                <hr />
+                <ButtonIcon icon='facebook' onClick={() => signIn('facebook', { callbackUrl: '/' })}>
+                    Login with Facebook
+                </ButtonIcon>
+                <ButtonIcon icon='login' onClick={() => signIn('github', { callbackUrl: '/' })}>
+                    Login with Github
+                </ButtonIcon>
+            </AccessibilityProvider>
             <ModalStatus theme='danger'>
                 {!!error && <>
                     <CardHeader>
