50b5672af20769f7cc47292b1fb0becf8cb07c28
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-08-05 04:17:00 +0700

    updated auth config

--- auth.config.ts
+++ auth.config.ts
@@ -6,6 +6,6 @@ export default {
 <p>To reset your password, click on the link below:<br />{{ResetLink}}</p>
 <p>Or copy and paste the URL into your browser:<br /><u>{{ResetLinkAsText}}</u></p>
 <p>If you did not make this request then please ignore this email.</p>`,
-    EMAIL_RESET_LIMITS  : 15,
-    EMAIL_RESET_MAX_AGE : 24,
+    EMAIL_RESET_LIMITS  : 15 /* minutes */,
+    EMAIL_RESET_MAX_AGE : 24 /* hours */,
 };
